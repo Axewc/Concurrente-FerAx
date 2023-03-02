6ba0654433ff1d59653bbe41e6b7366634177cef
Fergb01 <fer_gb@ciencias.unam.mx>
2023-03-02 16:38:57 -0600

    PrimeCalculator

--- Practica01/src/main/java/unam/ciencias/computoconcurrente/PrimeNumberCalculator.java
+++ Practica01/src/main/java/unam/ciencias/computoconcurrente/PrimeNumberCalculator.java
@@ -2,8 +2,8 @@ package unam.ciencias.computoconcurrente;
 import java.lang.Math;
 public class PrimeNumberCalculator implements Runnable{
 
-    private int threads;
-    private static int numPrimo;
+    int threads;
+    static int numPrimo;
     public static boolean result;
     public static int longitudSubInter; //Dividimos el intervalo [2,N-1] en this.threads cantidad de sub interbalos, uno por cada hilo
 
@@ -18,15 +18,35 @@ public class PrimeNumberCalculator implements Runnable{
     
 
     public boolean isPrime(int n) throws InterruptedException{
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
         return true;
-
     }
     
 
     @Override
     public void run(){
+        int inicio = 2 + (int) (Thread.currentThread().getId() * longitudSubInter);
+        int fin = inicio + longitudSubInter;
+        for (int i = inicio; i < fin; i++) {
+            try {
+                if (isPrime(i)) {
+                    numPrimo = i;
+                    result = true;
+                    break;
+                }
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
         
     }
 
-    
 }
